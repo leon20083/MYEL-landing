13eb2ebd0b0b0778dcfdceb736963fa4c985886d
leon20083 <44113374+leon20083@users.noreply.github.com>
2023-03-20 19:12:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ $(document).ready(function(){
         navText: ["<img src='img/arrow_left.png'>", "<img src='img/arrow_right.png'>"],
     });
   });
+
+  const buttons = document.getElementsByClassName('modal-button');
+  for(let button of buttons){
+    button.addEventListener('click', openModal);
+  }
+  const modalContainer = document.getElementById('modal__container');
+  modalContainer.addEventListener('click', closeModal);
+
+  function openModal(){
+    modalContainer.classList.add('active');
+  }
+
+  function closeModal(event){
+    if(event.target.id == 'modal__container'){
+        if(!modalContainer.classList.contains('active')) return;
+        modalContainer.classList.remove('active');
+    }
+  }
+
+  
