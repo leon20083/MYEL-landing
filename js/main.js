ef190810ad793986ab5f0ef6e976493942f09313
leon20083 <leon20083@mail.ru>
2023-03-20 07:27:23 +0300

    first commit new

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".slide-one").owlCarousel({
         loop: 1,
         items: 1,
         nav: true,
         navText: ["<img src='img/arrow_left.png'>", "<img src='img/arrow_right.png'>"],
     });
-    $(".browse__by__categories-photo").owlCarousel({
-        loop: false,
+    $(".slide-two").owlCarousel({
+        loop: true,
+        margin: 27,
         items: 3,
         nav: true,
+        dots: true,
         navText: ["<img src='img/arrow_left.png'>", "<img src='img/arrow_right.png'>"],
     });
   });
